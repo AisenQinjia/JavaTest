7eb89a3ea56c5270390274d1325f8039f1d11d12
unknown <Frank.Zhanghaochen@gmail.com>
2020-12-06 22:24:50 +0800

    add maven-build-plugin plugin to specify java version to 1.8

--- AOP-Test/src/main/java/org/example/zhc/aop/NeedProxy.java
+++ AOP-Test/src/main/java/org/example/zhc/aop/NeedProxy.java
@@ -1,14 +1,26 @@
 package org.example.zhc.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Slf4j
 @Component
 public class NeedProxy {
+    @Value(value = "ssss")
+    private String logInfo;
+
     public void a(){
-        log.info("a");
+        log.info(logInfo);
+        String[] arrayA = new String[3];
+        List<String> listA =  Arrays.asList(arrayA);
+        List<String> sList = new ArrayList<>();
+        String[] sArray =  sList.toArray(new String[0]);
     }
 
     @NeedProxyAnn
