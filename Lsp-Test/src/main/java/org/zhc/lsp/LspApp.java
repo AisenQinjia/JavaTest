857903f3fc7036bfd0eecc5c3b6e0b6f1844b2d7
艾森 <aisen@shangyoo.com>
2021-04-12 11:02:18 +0800

    adjust dependency

---
+++ Lsp-Test/src/main/java/org/zhc/lsp/LspApp.java
@@ -0,0 +1,28 @@
+package org.zhc.lsp;
+
+import org.example.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Parent{
+    public void doSomething(Map map){
+        Log.info("Parent do something");
+    }
+}
+//class Child extends Parent{
+////    public void doSomething(HashMap map){
+////        Log.info("Child do something1");
+////    }
+//
+////    @Override
+////    public void doSomething(Map map){
+////        Log.info("Child do something2");
+////    }
+//}
+public class LspApp {
+    public static void main(String[] args){
+        Parent child = new Parent();
+        child.doSomething(new HashMap());
+    }
+}
--- MongoDB-Test/src/main/java/org/example/zhc/mongo/MongoApp.java
+++ MongoDB-Test/src/main/java/org/example/zhc/mongo/MongoApp.java
@@ -11,6 +11,7 @@ import lombok.val;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.example.Log;
+import org.example.zhc.mongo.helper.Key;
 import org.example.zhc.mongo.helper.SubscriberHelpers;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
@@ -22,7 +23,9 @@ import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfigurati
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 
@@ -30,6 +33,7 @@ import java.util.function.Consumer;
 public class MongoApp implements CommandLineRunner {
     static CountDownLatch countDownLatch = new CountDownLatch(1);
     MongoClient mongoClient;
+    MongoDatabase database;
     public MongoCollection<Document> testCollection;
     public static void main(String[] args){
         Log.info("start mongo main");
@@ -88,9 +92,9 @@ public class MongoApp implements CommandLineRunner {
 
     public void update(){
         Mono.from(testCollection.updateOne(Filters.eq("main_id","22"),
-                Updates.combine(Updates.set("updateAdd","22")),
+                Updates.combine(Updates.set("updateAdd","pram"),Updates.set("isDel", true)),
                 new UpdateOptions().upsert(true))
-        ).flatMap(rel->Mono.just(rel.getModifiedCount())).subscribe();
+        ).subscribe(updateResult -> {Log.info("updateResult: %s",updateResult);},throwable -> Log.info("throwable: %s",throwable));
     }
 
     public void session(){
@@ -139,7 +143,29 @@ public class MongoApp implements CommandLineRunner {
         createIndex();
         update();
     }
+    /**
+     * 获取需要创建的索引
+     * @return 需要创建的索引
+     */
+    public List<IndexModel> getDefaultIndexes(){
+        List<IndexModel> defaultIndexes = new ArrayList<>();
+        //主键index
+        Bson primaryIndexBson = Indexes.compoundIndex(Indexes.hashed(Key.PRIMARY_KEY),Indexes.ascending((Key.DELETE_KEY)));
+        IndexModel primaryIndex = new IndexModel(primaryIndexBson);
+        //查询键
+        Bson queryIndexBson = Indexes.compoundIndex(Indexes.hashed(Key.QUERY_ALL_KEY),Indexes.ascending((Key.DELETE_KEY)));
+        IndexModel queryIndex = new IndexModel(queryIndexBson);
+        defaultIndexes.add(primaryIndex);
+        defaultIndexes.add(queryIndex);
+        return defaultIndexes;
+    }
 
+
+    public void checkCreateIndex(String collectionName){
+        Mono.from(database.getCollection(collectionName).createIndexes(getDefaultIndexes()))
+                .subscribe(s -> Log.info("create indexes: %s",s),throwable -> Log.info("create indexes error: %s", throwable));
+
+    }
     private <T> void subscribe(Publisher<T> publisher){
         val sub = new Subscriber<T>(){
             @Override
@@ -168,6 +194,8 @@ public class MongoApp implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Log.info("Application run!");
+
+//        "mongodb://root:lHmKKJc3r9@8.136.208.139:27017/?authSource=admin"
         ConnectionString connString = new ConnectionString(
                 "mongodb://root:H8oM55SbN4@10.100.2.90:27017/?authSource=admin"
         );
@@ -176,10 +204,15 @@ public class MongoApp implements CommandLineRunner {
                 .retryWrites(true)
                 .build();
         mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase("tpf_storage");
+        database = mongoClient.getDatabase("tpf_storage");
 
         testCollection = database.getCollection("test-collection2");
 
-        session();
+//        session();
+//        incr();
+//        update();
+//        checkCreateIndex("test-index-one");
     }
+
+
 }
