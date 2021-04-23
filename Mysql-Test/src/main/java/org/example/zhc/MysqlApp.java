2214acc53bdcfd8f5559f279f2f7688112fd5f56
艾森 <aisen@shangyoo.com>
2021-04-23 18:19:24 +0800

    sprint boot properties test

--- Mysql-Test/src/main/java/org/example/zhc/MysqlApp.java
+++ Mysql-Test/src/main/java/org/example/zhc/MysqlApp.java
@@ -8,14 +8,28 @@ public class MysqlApp {
     public static void main(String[] args){
 
     }
+    public static String homeConnect = "jdbc:mysql://localhost:3306/zhc";
+    public static String homeConnect_root = "root";
+    public static String homeConnect_password = "";
+
+    public static String companyConnect = "jdbc:mysql://10.100.2.54:30006/tpf_storage";
+    public static String companyConnect_root = "root";
+    public static String companyConnect_password = "password";
+
+    public static String connect = companyConnect;
+    public static String root = companyConnect_root;
+    public static String password = companyConnect_password;
+
+
     @Test
     public void mysql(){
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/zhc","root","");
+            Connection con = DriverManager.getConnection(connect,root,password);
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from emp");
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            ResultSet rs=stmt.executeQuery("select * from storage_50_0");
+            while(rs.next()){
+                System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
