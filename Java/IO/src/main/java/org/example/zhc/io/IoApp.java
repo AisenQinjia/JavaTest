ad5ce7067ec489585b449d6a178611b353909437
unknown <Frank.ZhangHaochen@gmail.com>
2021-05-05 13:02:46 +0800

    FileAndInputStream

---
+++ Java/IO/src/main/java/org/example/zhc/io/IoApp.java
@@ -0,0 +1,24 @@
+package org.example.zhc.io;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class IoApp {
+    public static void main(String[] args){
+
+    }
+    @Test
+    public void fileObj(){
+        File f1 = new File("foo");
+        System.out.println(f1.getAbsolutePath());
+        System.out.println(f1.exists() && f1.canRead());
+    }
+    @Test
+    public void fileReader() throws FileNotFoundException {
+        FileReader fr = new FileReader("/test");
+
+    }
+}
