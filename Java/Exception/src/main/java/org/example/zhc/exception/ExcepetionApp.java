279b9874baff1f2929ce0ed64ee748f80a390129
艾森 <aisen@shangyoo.com>
2021-05-11 21:04:06 +0800

    collection framework 1

---
+++ Java/Exception/src/main/java/org/example/zhc/exception/ExcepetionApp.java
@@ -0,0 +1,19 @@
+package org.example.zhc.exception;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * 异常处理测试
+ * @author zhanghaochen
+ */
+public class ExcepetionApp {
+    public static void main(String[] args){
+
+    }
+    @Test
+    public void ttt(){
+
+    }
+}
---
+++ Java/IterableOrMap/src/main/java/org/example/zhc/iom/IterableOrMapApp.java
@@ -0,0 +1,13 @@
+package org.example.zhc.iom;
+
+import org.junit.Test;
+
+public class IterableOrMapApp {
+    public static void main(String[] args){
+
+    }
+    @Test
+    public void simpleArray(){
+
+    }
+}
