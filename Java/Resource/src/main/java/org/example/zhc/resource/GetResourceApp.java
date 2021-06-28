9cde096f3152092c8c0bde6889b26cc5c70801be
艾森 <aisen@shangyoo.com>
2021-06-28 16:49:41 +0800

    Resource: add FileInputString test

--- Java/Resource/src/main/java/org/example/zhc/resource/GetResourceApp.java
+++ Java/Resource/src/main/java/org/example/zhc/resource/GetResourceApp.java
@@ -1,10 +1,21 @@
 package org.example.zhc.resource;
 
-import java.io.InputStream;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class GetResourceApp {
     public static void main(String[] args){
         InputStream url = GetResourceApp.class.getResourceAsStream("test");
         System.out.println("url "+url);
+    }
+    @Test
+    public void readTextFile() throws IOException {
+        InputStream fileReader = new FileInputStream("./test");
+        byte[] bytes =new byte[fileReader.available()];
+        fileReader.read(bytes);
+        String config =new String(bytes, StandardCharsets.UTF_8);
+        System.out.println("config: "+ config);
     }
 }
