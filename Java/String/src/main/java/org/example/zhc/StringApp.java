45ceb4fa846a74123f51373d95c5d4fac97a4d99
艾森 <aisen@shangyoo.com>
2021-08-17 20:31:35 +0800

    stringformatnulltest

--- Java/String/src/main/java/org/example/zhc/StringApp.java
+++ Java/String/src/main/java/org/example/zhc/StringApp.java
@@ -1,6 +1,10 @@
 package org.example.zhc;
 
 import org.junit.Test;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StringApp {
     @Test
@@ -10,5 +14,13 @@ public class StringApp {
         for (String s: strs){
             System.out.println(s);
         }
+    }
+
+    @Test
+    public void formatNull(){
+        Map<String,Object> o = null;
+        Map<String,Object> o1 = new HashMap<>();
+        o1.put("key", "value");
+        System.out.printf(String.format("%s  || %s", o, o1==null));
     }
 }
