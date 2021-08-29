bf2d75b75d79209c8f7bb0bba1f2098f59fa7eaa
unknown <Frank.ZhangHaochen@gmail.com>
2021-08-29 18:55:42 +0800

    factorbean

--- Spring/LifeCycle/src/main/java/org/example/zhc/SpringBootApp.java
+++ Spring/LifeCycle/src/main/java/org/example/zhc/SpringBootApp.java
@@ -1,6 +1,7 @@
 package org.example.zhc;
 
 import org.example.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +12,13 @@ import java.util.concurrent.CountDownLatch;
 @SpringBootApplication(scanBasePackages = "org.example")
 public class SpringBootApp implements CommandLineRunner {
     public static CountDownLatch countDownLatch = new CountDownLatch(1);
+    @Autowired
+    BeanByFactoryBean beanByFactoryBean;
     public static void main(String[] arg){
         try {
-            Log.info("SpringApplication.run Begin");
+            Log.info("=====================SpringApplication.run Begin=====================");
             SpringApplication.run(SpringBootApp.class);
-            Log.info("SpringAppli cation.run End");
+            Log.info("=====================SpringApplication.run End=====================");
             countDownLatch.await();
         }catch (Exception e){
             System.out.println("exception");
@@ -24,6 +27,7 @@ public class SpringBootApp implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-        Log.info("CommandLineRunner run");
+        Log.info("CommandLineRunner run ");
+        beanByFactoryBean.greeting();
     }
 }
