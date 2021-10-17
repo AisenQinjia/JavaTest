fb49b6a88cddd88f771ffd63ced6a5483b63b33b
unknown <Frank.ZhangHaochen@gmail.com>
2021-10-17 15:09:32 +0800

    tmp

--- Java/String/src/main/java/org/example/zhc/StringApp.java
+++ Java/String/src/main/java/org/example/zhc/StringApp.java
@@ -1,7 +1,6 @@
 package org.example.zhc;
 
 import org.junit.Test;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,5 +21,24 @@ public class StringApp {
         Map<String,Object> o1 = new HashMap<>();
         o1.put("key", "value");
         System.out.printf(String.format("%s  || %s", o, o1==null));
+    }
+
+    @Test
+    public void divisionOpt(){
+        long a,b;
+        long  t1 = System.currentTimeMillis();
+        for (long val = 0; val < 10000000; val += 5){
+            a = val * 8;
+            b = val/2;
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println("before opt(/mills): "+ (t2 - t1)/1);
+        for (long val = 0; val < 10000000; val += 5){
+            a = val << 8;
+            b = val >> 2;
+        }
+        long t3 = System.currentTimeMillis();
+        System.out.println("after opt(/mills): "+(t3 - t2)/1);
+        System.out.println("diff: "+(2*t2 - t1 - t3)/1);
     }
 }
