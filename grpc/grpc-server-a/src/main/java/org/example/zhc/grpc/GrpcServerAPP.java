39d0cb919c059f7bf443f3152982bd9ed31cbeac
艾森 <aisen@shangyoo.com>
2021-12-18 15:09:27 +0800

    grpc

--- grpc/grpc-server-a/src/main/java/org/example/zhc/grpc/GrpcServerAPP.java
+++ grpc/grpc-server-a/src/main/java/org/example/zhc/grpc/GrpcServerAPP.java
@@ -28,6 +28,65 @@ public class GrpcServerAPP {
             responseObserver.onCompleted();
         }
 
+        @Override
+        public void serverStreamCall(Req request, StreamObserver<Rsp> responseObserver) {
+            log.info("client serverStreamCall");
+            Rsp rsp = Rsp.newBuilder()
+                    .setMessage("sever resp1!")
+                    .build();
+            Rsp rsp2 = Rsp.newBuilder()
+                    .setMessage("sever resp!2")
+                    .build();
+            responseObserver.onNext(rsp);
+            responseObserver.onNext(rsp2);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public StreamObserver<Req> clientStreamCall(StreamObserver<Rsp> responseObserver) {
+            return new StreamObserver<Req>() {
+                @Override
+                public void onNext(Req req) {
+                    log.info("onNext req");
+
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    log.error("error: ",throwable);
+                }
+
+                @Override
+                public void onCompleted() {
+                    log.info("on Completed");
+                    responseObserver.onNext(Rsp.newBuilder().build());
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+
+        @Override
+        public StreamObserver<Req> biStreamCall(StreamObserver<Rsp> responseObserver) {
+            return new StreamObserver<Req>() {
+                @Override
+                public void onNext(Req req) {
+                    log.info("onNext");
+                    responseObserver.onNext(Rsp.newBuilder().build());
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+
+                }
+
+                @Override
+                public void onCompleted() {
+                    log.info("onCompleted");
+                    responseObserver.onNext(Rsp.newBuilder().build());
+                }
+            };
+        }
+
         public GrpcServerA(int port){
             this.port = port;
             server = ServerBuilder.forPort(port).addService(this).build();
