669508bfedd508ca142ab2ded8766d27ee5fa359
艾森 <aisen@shangyoo.com>
2022-02-15 14:55:14 +0800

    generics

--- Java/Generic/src/main/java/org/example/zhc/WithGenericClass.java
+++ Java/Generic/src/main/java/org/example/zhc/WithGenericClass.java
@@ -1,7 +1,17 @@
 package org.example.zhc;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class WithGenericClass {
+public class WithGenericClass<T> {
     List<String> stringList;
+    public void add(){
+        Number a = 1;
+        Class<? extends Number> nc = a.getClass();
+        Number[] ns = Arrays.copyOf(new Integer[0], 0,Number[].class);
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return Arrays.copyOf(new Integer[0], 0, (Class<? extends T[]>) a.getClass());
+    }
 }
