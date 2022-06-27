59800469b445feff10d39483f3c8538e0f8857d9
艾森 <aisen@shangyoo.com>
2022-06-27 21:36:04 +0800

    sublist test

---
+++ Spring/spring-data-redis/src/main/java/org/example/zhc/spring/redis/RedisTestComponent.java
@@ -0,0 +1,42 @@
+package org.example.zhc.spring.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisTestComponent implements CommandLineRunner {
+    @Autowired
+    ReactiveStringRedisTemplate redisTemplate;
+    @Autowired
+    StreamListenerExample listener;
+    @Autowired
+    RedisConnectionFactory connectionFactory;
+    @Override
+    public void run(String... args){
+        StreamMessageListenerContainer.StreamMessageListenerContainerOptions<String, MapRecord<String, String, byte[]>> containerOptions =
+                StreamMessageListenerContainer.StreamMessageListenerContainerOptions
+                        .builder()
+                        .keySerializer(StringRedisSerializer.UTF_8)
+                        .<String, byte[]>hashKeySerializer(StringRedisSerializer.UTF_8)
+                        .<String, byte[]>hashValueSerializer(RedisSerializer.byteArray())
+                        .pollTimeout(Duration.ofMillis(1000))
+                        .build();
+        StreamMessageListenerContainer<String, MapRecord<String, String, byte[]>> container = StreamMessageListenerContainer.create(connectionFactory,
+                containerOptions);
+        long l = System.currentTimeMillis();
+        String entryId = l + "-0";
+        container.receive(StreamOffset.create("my-stream2",ReadOffset.lastConsumed()), listener);
+        container.start();
+    }
+}
---
+++ Spring/spring-data-redis/src/main/java/org/example/zhc/spring/redis/StreamListenerExample.java
@@ -0,0 +1,18 @@
+package org.example.zhc.spring.redis;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class StreamListenerExample implements StreamListener<String, MapRecord<String, String, byte[]>> {
+    @Override
+    public void onMessage(MapRecord<String, String, byte[]> entries) {
+        log.info("entries: stream:{} id:{} val:{} ",entries.getStream(),entries.getId(),entries.getValue());
+    }
+}
